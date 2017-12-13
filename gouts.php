9d64a26ad90b15c7bbe1b0d40b3348fad3c45ca7
cegaz <cecilegaz@gmail.com>
2017-12-13 14:44:54 +0100

    test

--- gouts.php
+++ gouts.php
@@ -12,14 +12,14 @@
 
 	$proposition = "...";
 
-	/*if($box == 'oui') {
+	if($box == 'oui') {
 		if($thon == 'oui') {
 			$proposition = 'la Sushi Box Salmon Lovers (https://www.sushishop.fr/fr/livraison/plateaux/salmon-lovers-678)';
 		} else {
 			$proposition = 'la Sushi Box Super Mix (https://www.sushishop.fr/fr/livraison/plateaux/super-mix-1047)'
 		}
 	} else{
-		if($explorateur == 'oui') {
+		if($nouveaute == 'oui') {
 			$proposition = 'notre nouveauté 2017 : Sushi Black Pepper (https://www.sushishop.fr/fr/livraison/sushi/black-pepper-64)'
 		} else {
 			$proposition = 'notre classique : le sushi saumon';
@@ -27,6 +27,6 @@
 
 	if($epice == 'oui') {
 		$proposition .= ', avec un supplément Wasabi Peas';
-	}*/
+	}
 
-//	$speech .= $proposition . "Est-ce que cela vous correspond ?";
+	$speech .= $proposition . "Est-ce que cela vous correspond ?";
