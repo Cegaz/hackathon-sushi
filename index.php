9b6613e06536b411c86e0904bdf976e0e8be2692
cegaz <cecilegaz@gmail.com>
2017-12-13 12:16:00 +0100

    test

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ if($method = "POST"){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	if(isset($json->result->parameters->{'oui_non'})) {
+	if(isset($json->result->parameters->'oui_non')) {
 		include('oui_non.php');
 	}
 
@@ -14,9 +14,9 @@ if($method = "POST"){
 		include('commande.php');
 	}
 
-	if(isset($json->result->parameters->{'explorateu'})) {
+	if(isset($json->result->parameters->{'explorateur'})) {
 		include('gouts.php');
-	})
+	}
 
 	$response = new \stdClass();
 	$response->speech = $speech;
