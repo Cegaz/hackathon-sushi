a81ba8214e43221ab4ca29c0aed8a8d14e93eeb7
cegaz <cecilegaz@gmail.com>
2017-12-13 14:52:51 +0100

    test

--- gouts.php
+++ gouts.php
@@ -8,18 +8,18 @@
 
 
 	$speech = "OK. Je crois que je commence à vous cerner... Voilà ma suggestion pour vous : ";
-/*
+
 	$proposition = "...";
 
 	if($box == 'oui') {
 		if($thon == 'oui') {
 			$proposition = 'la Sushi Box Salmon Lovers (https://www.sushishop.fr/fr/livraison/plateaux/salmon-lovers-678)';
 		} else {
-			$proposition = 'la Sushi Box Super Mix (https://www.sushishop.fr/fr/livraison/plateaux/super-mix-1047)'
+			$proposition = 'la Sushi Box Super Mix (https://www.sushishop.fr/fr/livraison/plateaux/super-mix-1047)';
 		}
 	} else{
 		if($nouveaute == 'oui') {
-			$proposition = 'notre nouveauté 2017 : Sushi Black Pepper (https://www.sushishop.fr/fr/livraison/sushi/black-pepper-64)'
+			$proposition = 'notre nouveauté 2017 : Sushi Black Pepper (https://www.sushishop.fr/fr/livraison/sushi/black-pepper-64)';
 		} else {
 			$proposition = 'notre classique : le sushi saumon';
 		}
@@ -28,4 +28,4 @@
 		$proposition .= ', avec un supplément Wasabi Peas';
 	}
 
-	$speech .= $proposition . "Est-ce que cela vous correspond ?";*/
+	$speech .= $proposition . "Est-ce que cela vous correspond ?";
