6c2a8ec8307dbfd04ce68d76a04bf1e07b667ef8
cegaz <cecilegaz@gmail.com>
2017-12-13 10:40:54 +0100

    test

--- index.php
+++ index.php
@@ -6,15 +6,14 @@ if($method = "POST"){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 
-	if(isset($json->result->parameters->poisson)){
+	/*if(isset($json->result->parameters->poisson)){
 		($commande['poisson'] = $json->result->parameters->poisson;
 		$commande['accompagnement'] = $json->result->parameters->accompagnement;
 		$commande['sauce'] = $json->result->parameters->sauce;
 		$commande['type'] = $json->result->parameters->type;
 
-//		$speech = "j'ai bien noté votre commande : vous voulez des " $commande['type'] . " de ". $commande['poisson'] . " avec du " . $commande['accompagnement'] . ", sauce " . $commande['sauce'] . " : c'est bien cela ?";
-		$speech = 'ok commande';
-	}
+	$speech = "j'ai bien noté votre commande : vous voulez des " $commande['type'] . " de ". $commande['poisson'] . " avec du " . $commande['accompagnement'] . ", sauce " . $commande['sauce'] . " : c'est bien cela ?";
+	}*/
 
 	if(isset($json->result->parameters->oui_non)){
 		$oui_non = $json->result->parameters->oui_non;
@@ -36,7 +35,6 @@ if($method = "POST"){
 				$speech = "je n'ai pas compris votre demande";
 				break;
 		}
-		$speech = "oui non ok";
 	}
 
 	$response = new \stdClass();
