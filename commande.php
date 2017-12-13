37559586610ed8b8f7b1e862c753dc5144a77a7c
ratchi81 <clement.jourdan81@gmail.com>
2017-12-13 12:43:40 +0100

    Page commande

--- commande.php
+++ commande.php
@@ -1,6 +1,4 @@
 <?php
-include 'connect.php';
-$pdo = new PDO(DSN, USER, PASS);
 
 $poisson = $json->result->parameters->{'poisson'};
 $accompagnement = $json->result->parameters->{'accompagnement'};
@@ -10,6 +8,9 @@ $type = $json->result->parameters->{'type'};
 $speech = "j'ai bien noté votre commande : vous voulez des " . $type . " de ". $poisson . " avec du " . $accompagnement . ", sauce " . $sauce . " : c'est bien cela ?";
 /*
 if(isset($speech)) {
+include 'connect.php';
+$pdo = new PDO(DSN, USER, PASS);
+
     $sql_up = "UPDATE commande SET 
                 poisson = :poisson, 
                 accompagnement = :accompagnement, 
