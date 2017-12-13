462d7695542caa2acdbb838c19b8489e2343a3f4
cegaz <cecilegaz@gmail.com>
2017-12-13 10:58:05 +0100

    test

--- commande.php
+++ commande.php
@@ -1,10 +1,10 @@
 <?
 
-	//$poisson = $json->result->parameters->poisson;
+	$poisson = $json->result->parameters->poisson;
 	/*$accompagnement = $json->result->parameters->accompagnement;
 	$sauce = $json->result->parameters->sauce;
 	$type = $json->result->parameters->type;
 
 	$speech = "j'ai bien noté votre commande : vous voulez des " $type . " de ". $poisson . " avec du " . $accompagnement . ", sauce " . $sauce . " : c'est bien cela ?";
 	*/
-	$speech = "vous aimez le poisson";
+	$speech = "vous aimez le " . $poisson;
