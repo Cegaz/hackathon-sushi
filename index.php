96f58515cae093f53b2479e4981d29ee765ea106
cegaz <cecilegaz@gmail.com>
2017-12-13 10:08:04 +0100

    test

--- index.php
+++ index.php
@@ -22,6 +22,26 @@ if($method = "POST"){
 			break;
 	}
 
+	$oui_non = $json->result->parameters->oui_non;
+
+	switch ($oui_non) {
+		case 'oui':
+			$speech = 'ok, commençons ensemble';
+			break;
+
+		case 'non':
+			$speech = 'c\'est noté, une prochaine fois peut-être';
+			break;
+
+		case 'peut-etre':
+			$speech = "ok, je vous laisse réfléchir";
+			break;
+		
+		default:
+			$speech = "je n'ai pas compris votre demande";
+			break;
+	}
+
 	$response = new \stdClass();
 	$response->speech = $speech;
 	$response->displayText = $speech;
